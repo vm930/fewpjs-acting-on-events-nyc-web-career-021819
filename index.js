528e2b58a191420a751e01a24542a1cf45ac9e0e
Victoria Mei <vm930@hotmail.com>
2019-04-01 23:21:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left + 1}px`;
+}
